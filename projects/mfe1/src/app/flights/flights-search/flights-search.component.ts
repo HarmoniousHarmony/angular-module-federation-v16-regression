d587f26eedc4543dc4cc8d8994e2c7a76045a4c7
Hadi Amiri <hadi.amiri@relexsolutions.com>
2023-08-04 11:09:30 +0300

    5

--- projects/mfe1/src/app/flights/flights-search/flights-search.component.ts
+++ projects/mfe1/src/app/flights/flights-search/flights-search.component.ts
@@ -1,12 +1,24 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import * as signalR from '@microsoft/signalr';
 
 @Component({
   selector: 'app-flights-search',
   templateUrl: './flights-search.component.html',
-  styleUrls: ['./flights-search.component.css']
+  styleUrls: ['./flights-search.component.css'],
 })
 export class FlightsSearchComponent {
+  connection: signalR.HubConnection;
+  constructor() {
+    this.connection = new signalR.HubConnectionBuilder()
+      .withUrl('/chat')
+      .build();
 
+    this.connection.on('send', (data) => {
+      console.log(data);
+    });
+
+    this.connection.start().then(() => this.connection.invoke('send', 'Hello'));
+  }
   search(): void {
     alert('Not implemented for this demo!');
   }
@@ -14,5 +26,4 @@ export class FlightsSearchComponent {
   terms(): void {
     alert('Not implemented for this demo!');
   }
-
 }
--- projects/mfe1/webpack.config.js
+++ projects/mfe1/webpack.config.js
@@ -1,21 +1,21 @@
 const {
   shareAll,
   withModuleFederationPlugin,
-} = require('@angular-architects/module-federation/webpack');
+} = require("@angular-architects/module-federation/webpack");
 
 module.exports = withModuleFederationPlugin({
-  name: 'mfe1',
+  name: "mfe1",
 
   exposes: {
     // Update this whole line (both, left and right part):
-    './Module': './projects/mfe1/src/app/flights/flights.module.ts',
+    "./Module": "./projects/mfe1/src/app/flights/flights.module.ts",
   },
 
   shared: {
     ...shareAll({
       singleton: true,
       strictVersion: true,
-      requiredVersion: 'auto',
+      requiredVersion: "auto",
     }),
   },
 });
